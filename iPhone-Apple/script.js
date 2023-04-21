576edaa006603ad6b1db6519129a52cfa5b2f322
Gabriel Mendes <contatogabrielmendesgoncalves@gmail.com>
2023-04-21 13:40:22 -0300

    IphoneClonePage

---
+++ iPhone-Apple/script.js
@@ -0,0 +1,11 @@
+const btnMobile = document.getElementById("btn-mobile");
+
+function abrir(event) {
+  if ( event.type === "touchstart") event.preventDefault();
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active")
+}
+
+
+btnMobile.addEventListener("click", abrir)
+btnMobile.addEventListener("touchstart", abrir)
